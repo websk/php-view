4357d8408939a2869925cd45136accfe94db5b5b
skulkov <sergey.kulkov@gmail.com>
2018-12-07 10:22:30 +0300

    renderLocalTemplate

--- src/WebSK/Views/PhpRender.php
+++ src/WebSK/Views/PhpRender.php
@@ -63,12 +63,12 @@ class PhpRender
 
     /**
      * @param string $template
-     * @param array $variables
+     * @param array $data
      * @return string
      */
-    public static function renderTemplate(string $template, array $variables = [])
+    public static function renderTemplate(string $template, array $data = [])
     {
-        extract($variables, EXTR_SKIP);
+        extract($data, EXTR_SKIP);
         ob_start();
 
         require ViewsPath::getFullTemplatePath($template);
@@ -82,24 +82,54 @@ class PhpRender
     /**
      * @param string $module
      * @param string $template
-     * @param array $variables
+     * @param array $data
      * @return false|string
      */
-    public static function renderTemplateByModule(string $module, string $template, array $variables = [])
+    public static function renderTemplateByModule(string $module, string $template, array $data = [])
     {
         $site_modules_file_path = ViewsPath::VIEWS_MODULES_DIR . DIRECTORY_SEPARATOR . $module . DIRECTORY_SEPARATOR . $template;
 
         if (!file_exists(ViewsPath::getFullTemplatePath($site_modules_file_path))) {
             return self::renderTemplate(
                 $site_modules_file_path,
-                $variables
+                $data
             );
         }
 
-        extract($variables, EXTR_SKIP);
+        extract($data, EXTR_SKIP);
         ob_start();
 
         require ViewsPath::getSiteSrcPath() . DIRECTORY_SEPARATOR . $module . DIRECTORY_SEPARATOR . ViewsPath::VIEWS_DIR_NAME . DIRECTORY_SEPARATOR . $template;
+
+        $contents = ob_get_contents();
+
+        ob_end_clean();
+
+        return $contents;
+    }
+
+    /**
+     * @param string $template
+     * @param array $data
+     * @return false|string
+     * @throws \Exception
+     */
+    public static function renderLocalTemplate(string $template, array $data = []) {
+        $cb_arr = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT);
+        $caller_obj = array_shift($cb_arr);
+        Assert::assert($caller_obj);
+
+        $caller_path = $caller_obj['file'];
+        $caller_path_arr = pathinfo($caller_path);
+
+        $caller_dir = $caller_path_arr['dirname'];
+
+        $full_template_path = $caller_dir . DIRECTORY_SEPARATOR . ViewsPath::VIEWS_DIR_NAME . DIRECTORY_SEPARATOR . $template;
+
+        extract($data, EXTR_SKIP);
+        ob_start();
+
+        require $full_template_path;
 
         $contents = ob_get_contents();
 
